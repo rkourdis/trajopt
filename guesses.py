Use implicit Euler and correct time averaged dynamics (similar to Posa '13, but without the LCP).

guesses.py
@@ -21,12 +21,8 @@ def const_pose_guess(n_knots: int, fk: ADFootholdKinematics, pose: Pose = None) 
     a0 = np.zeros((fk.cmodel.nv, 1))                      
 
     # Create numerical instance of FK to calculate feet kinematics:
-    q_sym, v_sym, a_sym = \
-        ca.SX.sym("q_sym", q0.shape), ca.SX.sym("v_sym", v0.shape), ca.SX.sym("a_sym", a0.shape)
-    
-    f_pos_0, f_vel_0, f_acc_0 = ca.Function(
-        "num_fk_pos", [q_sym, v_sym, a_sym], fk(q_sym, v_sym, a_sym)
-    )(q0, v0, a0)
+    q_sym = ca.SX.sym("q_sym", q0.shape)
+    f_pos_0 = ca.Function("num_fk_pos", [q_sym], [fk(q_sym)])(q0)
 
     return Trajectory(
         num_knots   = n_knots,
@@ -36,8 +32,6 @@ def const_pose_guess(n_knots: int, fk: ADFootholdKinematics, pose: Pose = None) 
         tau_k       = [np.copy(tau0) for _ in range(n_knots)],
         λ_k         = [np.copy(λ0) for _ in range(n_knots)],
         f_pos_k     = [np.copy(f_pos_0) for _ in range(n_knots)],
-        f_vel_k     = [np.copy(f_vel_0) for _ in range(n_knots)],
-        f_acc_k     = [np.copy(f_acc_0) for _ in range(n_knots)]
     )
 
 # Loads a previous solution as an initial guess trajectory.

tasks.py
@@ -1,15 +1,15 @@
 import math
 import numpy as np
 import casadi as ca
-from typing import Callable
 from itertools import chain
+from typing import Callable, Union
 
 import intervaltree as ivt
 from dataclasses import dataclass
 
 from utilities import ε
-from transcription import Constraint
 from poses import Pose, load_robot_pose
+from transcription import Constraint, Bound
 
 @dataclass
 class Task:
@@ -27,19 +27,20 @@ class Task:
     traj_error: Callable[[float, ca.SX, ca.SX, ca.SX, ca.SX, ca.SX], float]     
 
     # Given the kinematic decision variables (q_k, v_k, α_k, f_pos_k), return a list
-    # of constraints that a solution to the task needs to satisfy: 
+    # of constraints or variable bounds that a task solution needs to satisfy: 
     get_kinematic_constraints: Callable[
-        [list[ca.SX], list[ca.SX], list[ca.SX], list[ca.SX]], list[Constraint]
+        [list[ca.SX], list[ca.SX], list[ca.SX], list[ca.SX]],
+        list[Union[Constraint, Bound]]
     ]
 
 JUMP_TASK: Task = Task(
     name = "jump",
-    duration = 2.0,
+    duration = 1.0,
 
     contact_periods = [
         ivt.IntervalTree([
             ivt.Interval(0.0, 0.3 + ε),
-            ivt.Interval(0.7, 2.0 + ε)
+            ivt.Interval(0.6, 1.0 + ε)
         ])
         for _ in range(4)
     ],
@@ -52,112 +53,13 @@ JUMP_TASK: Task = Task(
         Constraint(q_k[0] - load_robot_pose(Pose.STANDING_V)[0]),
 
         # Torso has moved forward by end of jump:
-        Constraint(q_k[-1][0], 0.4, ca.inf),
+        Bound(q_k[-1][0], 0.4, ca.inf),
 
         # Torso is above the ground at a certain height at the end:
-        Constraint(q_k[-1][2], lb = params["FLOOR_Z"] + 0.2, ub = ca.inf),
+        Bound(q_k[-1][2], params["FLOOR_Z"] + 0.2, ca.inf),
 
         # The entire robot is static at the beginning and end:
-        Constraint(v_k[0]),
-        Constraint(v_k[-1])
-    ]
-)
-
-BACKFLIP_LAND_TASK: Task = Task(
-    name = "backflip_land",
-    duration = 1.0,
-
-    # Flip will last 600ms:
-    contact_periods = [
-        ivt.IntervalTree([ivt.Interval(0.3, 1.0 + ε)]),  # FR
-        ivt.IntervalTree([ivt.Interval(0.3, 1.0 + ε)]),  # FL
-        ivt.IntervalTree([ivt.Interval(0.35, 1.0 + ε)]), # HR
-        ivt.IntervalTree([ivt.Interval(0.35, 1.0 + ε)]), # HL
-    ],
-
-    # Minimize joint velocity when airborne, then contact forces and orientation:
-    traj_error = \
-        lambda t, q, v, a, τ, λ: 
-            # ca.sqrt(τ.T @ τ if t < 0.3 else ca.sum1(ca.diag(λ @ λ.T))),
-            ca.sqrt(v[6:].T @ v[6:]         
-                if t < 0.3                  
-            else ca.sum1(ca.diag(λ @ λ.T))), # + 12.0 * q[3:6].T @ q[3:6],
-
-    get_kinematic_constraints = lambda q_k, v_k, a_k, f_pos_k, params: [
-        Constraint(q_k[0][0], lb = -0.2, ub = -0.2),      # We give it 40cm backwards slack
-        Constraint(q_k[0][1], lb = 0.0,  ub = 0.0),
-        Constraint(q_k[0][2], lb = 0.4,  ub = 0.4),       # 0.44145m for 600deg/s rotation rate
-        Constraint(q_k[0][3:6] - ca.SX([0.0, 1.0, 0.0])), # Switched MRP
-
-        # Feet configuration mid-flip
-        Constraint(q_k[0][6:] - load_robot_pose(Pose.STANDING_V)[0][6:]),
-
-        # Torso has moved backwards at touchdown:
-        Constraint(q_k[math.ceil(0.3 * params["FREQ_HZ"])][0], lb = -0.4, ub = -0.4),
-
-        # # Orientation is horizontal at touchdown:
-        # Constraint(q_k[math.ceil(0.3 * params["FREQ_HZ"])][3:6]),
-
-        # Flipping velocity:
-        Constraint(v_k[0][3], lb = 0.0,     ub = 0.0),
-        Constraint(v_k[0][4], lb = -ca.inf, ub = 0.0),
-        Constraint(v_k[0][5], lb = 0.0,     ub = 0.0),
-
-        # # Torso is above the ground at a certain height at the end:
-        # Constraint(q_k[-1][2], lb = params["FLOOR_Z"] + 0.2, ub = ca.inf),
-
-        # The entire robot is static and horizontal at the end. Legs in V configuration.
-        Constraint(q_k[-1][3:6]),
-        Constraint(v_k[-1]),
-        Constraint(q_k[-1][6:] - load_robot_pose(Pose.STANDING_V)[0][6:]),
-
-        # # Front legs are in front of back legs, by a bit!
-        # Constraint(f_pos_k[-1][0, 0] - f_pos_k[-1][2, 0], lb = 0.1, ub = ca.inf),
-        # Constraint(f_pos_k[-1][1, 0] - f_pos_k[-1][3, 0], lb = 0.1, ub = ca.inf),
-    ] + \
-    
-    
-    list(
-        chain.from_iterable([
-            [
-                # Keep FR/FL HFE at less than fully folded as there's the joint stopper:
-                Constraint(q_k[k][7], lb = -ca.inf, ub = np.deg2rad(85)),
-                Constraint(q_k[k][10], lb = -ca.inf, ub = np.deg2rad(85)),
-
-                # Keep the front / hind two knees at an angle of less than 125 / 120 deg after the landing.
-                # This is a hack to make sure the knees don't go below the ground :) 
-                Constraint(q_k[k][8], lb =  -np.deg2rad(125), ub = ca.inf),     # Front
-                Constraint(q_k[k][11], lb = -np.deg2rad(125), ub = ca.inf),
-                Constraint(q_k[k][14], lb = -ca.inf, ub = np.deg2rad(120)),     # Hind
-                Constraint(q_k[k][17], lb = -ca.inf, ub = np.deg2rad(120))
-            ]
-            for k in range(math.ceil(0.3 * params["FREQ_HZ"]), params["N_KNOTS"])
-        ]
-    ))
-)
-
-BACKFLIP_LAUNCH_TASK: Task = Task(
-    # TODO: This is a work in progress
-    
-    name = "backflip_launch",
-    duration = 1.0,
-
-    contact_periods = [
-        ivt.IntervalTree([
-            ivt.Interval(0.0, 0.5 + ε),
-            ivt.Interval(0.7, 1.0 + ε)
-        ])
-        for _ in range(4)
-    ],
-
-    # RMS of actuation torque:
-    traj_error = lambda t, q, v, a, τ, λ: ca.sqrt(τ.T @ τ),
-
-    get_kinematic_constraints = lambda q_k, v_k, a_k, f_pos_k, params: [
-        Constraint(q_k[0] - load_robot_pose(Pose.STANDING_V)[0]),
-        Constraint(q_k[-1] - load_robot_pose(Pose.STANDING_V)[0]),
-
-        Constraint(v_k[0]),
-        Constraint(v_k[-1])
+        Bound(v_k[0]),
+        Bound(v_k[-1]),
     ]
 )

transcription.py
@@ -16,14 +16,11 @@ class Trajectory:
     tau_k: list[np.array]   = None    # 12x1
     λ_k: list[np.array]     = None    # 4x3
     f_pos_k: list[np.array] = None    # 4x3
-    f_vel_k: list[np.array] = None    # 4x3
-    f_acc_k: list[np.array] = None    # 4x3
 
     def flatten(self) -> ca.DM:
         return ca.vertcat(
             flatten(self.q_k), flatten(self.v_k), flatten(self.a_k),
-            flatten(self.tau_k), flatten(self.λ_k),
-            flatten(self.f_pos_k), flatten(self.f_vel_k), flatten(self.f_acc_k)
+            flatten(self.tau_k), flatten(self.λ_k), flatten(self.f_pos_k)
         )
     
     # Interpolate by simple repetition of knots:
@@ -47,8 +44,6 @@ class Trajectory:
         result.tau_k = repeat(self.tau_k)
         result.λ_k = repeat(self.λ_k)
         result.f_pos_k = repeat(self.f_pos_k)
-        result.f_vel_k = repeat(self.f_vel_k)
-        result.f_acc_k = repeat(self.f_acc_k)
 
         return result
 
@@ -74,41 +69,51 @@ class Trajectory:
         o, sz = o + sz * num_knots, 4 * 3
         traj.f_pos_k = unflatten(vec[o : o + num_knots * sz], (4, 3))
 
-        o, sz = o + sz * num_knots, 4 * 3
-        traj.f_vel_k = unflatten(vec[o : o + num_knots * sz], (4, 3))
-
-        o, sz = o + sz * num_knots, 4 * 3
-        traj.f_acc_k = unflatten(vec[o : o + num_knots * sz], (4, 3))
-
         return traj
-    
+
 @dataclass
 class Constraint:
+    # Elementwise constraint: lb <= expr[:] <= ub
     expr:   ca.SX
     lb:     np.array
     ub:     np.array
 
-    # Create a new elementwise constraint: lb <= expr[:] <= ub
     def __init__(self, expr: ca.SX, lb: float = 0.0, ub: float = 0.0):
         assert lb <= ub
         self.expr, self.lb, self.ub = expr, np.full(expr.shape, lb), np.full(expr.shape, ub)
+
+@dataclass
+class Bound:
+    # Elementwise variable bound: lb <= variables[:] <= ub
+    variables:  ca.SX
+    lb:         float   = 0.0
+    ub:         float   = 0.0
+
+    def __post_init__(self):
+        assert self.lb <= self.ub
+        
+        for i in range(self.variables.shape[0]):
+            for j in range(self.variables.shape[1]):
+                assert self.variables[i,j].is_symbolic(), \
+                    "Bound expression contains non-leaf entries"
+
 
 class VariableBounds:
     def __init__(self):
         self.bounds: dict[str, tuple[float, float]] = {}
 
-    def add_expr(self, expr: ca.SX, lb: float, ub: float):
-        assert lb <= ub, "Lower bound must be <= upper bound"
-
-        for i in range(expr.shape[0]):
-            for j in range(expr.shape[1]):
-                var = expr[i, j]
-                assert var.is_symbolic(), "Bound expression contains non-leaf entries"
+    def add(self, bound: Bound):
+        for i in range(bound.variables.shape[0]):
+            for j in range(bound.variables.shape[1]):
+                var = bound.variables[i, j]
                 
                 # If there's an existing bound, make it tighter:
-                existing_bound = self.get_bounds(var.name())
-                self.bounds[var.name()] = (max(lb, existing_bound[0]), min(ub, existing_bound[1]))
+                existing_bound = self.get_bound(var.name())
+                self.bounds[var.name()] = (
+                    max(bound.lb, existing_bound[0]),
+                    min(bound.ub, existing_bound[1])
+                )
     
-    def get_bounds(self, var_name: str) -> tuple[float, float]:
+    def get_bound(self, var_name: str) -> tuple[float, float]:
         return b if (b := self.bounds.get(var_name)) else (-ca.inf, ca.inf)
     